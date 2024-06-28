acfe37a639ce656196c7178a84827954fa8492e8
Margarida Grilo <margaridagrilo7@gmail.com>
2024-06-28 01:32:34 +0100

    new deploy branch: render

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:3003/api',
+	baseURL: 'https://bubblemap.onrender.com/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
